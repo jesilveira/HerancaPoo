d3cbe49ce35c66941be4becff464a1a86ff8e474
Jessika <jessika.santos.silveira@gmail.com>
2022-06-14 11:05:07 -0300

    Projeto Heranca

---
+++ src/Funcionario.java
@@ -0,0 +1,32 @@
+
+public class Funcionario extends Pessoa {
+
+	private String setor;
+	private String trabalhando;
+	
+	
+	
+	
+	public void mudarTrabalho() {
+		
+	}
+	
+	
+	public String getSetor() {
+		return setor;
+	}
+	public void setSetor(String setor) {
+		this.setor = setor;
+	}
+	public String getTrabalhando() {
+		return trabalhando;
+	}
+	public void setTrabalhando(String trabalhando) {
+		this.trabalhando = trabalhando;
+	}
+	
+	
+	
+	
+	
+}
---
+++ src/Main.java
@@ -0,0 +1,27 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Pessoa p1 = new Pessoa();
+		Aluno p2 = new Aluno();
+		Professor p3 = new Professor();
+		Funcionario p4 = new Funcionario();
+		
+		
+		p1.setNome("Juliana");
+		p2.setNome("Angelo");
+		p3.setNome("Diego");
+		p4.setNome("Sofia");
+		
+		p2.setCurso("Lógica");
+		p3.setSalario(2.500d);
+		p4.setSetor("Estoque");
+		
+		
+		
+		
+		
+	}
+
+}
